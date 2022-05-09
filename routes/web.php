b42940e33990ee0378ca8f57b511466b061e091e
Adam Bosiacki <Bosiacki.Adam@gmail.com>
2022-05-09 18:59:47 +0200

    Nowy dashboard i wylogowanie

--- routes/web.php
+++ routes/web.php
@@ -124,3 +124,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/logout', [App\Http\Controllers\LogOut::class, 'logout']);
